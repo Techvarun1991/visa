63cbc53dd27e1015d70848ce2d6ea0aa03653c5b
Saurabh Tajane <saurabhtajne07@gmail.com>
2024-09-14 10:50:21 +0530

    Cors cofig file added in gateway

---
+++ api-gateway/src/main/java/com/micro/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.micro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    // Use allowedOriginPatterns instead of allowedOrigins
+    corsConfiguration.addAllowedOriginPattern("*");
+    // Specify the origins explicitly if you know them
+    corsConfiguration.addAllowedOrigin("http://localhost:3000");
+    corsConfiguration.addAllowedMethod("*");
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+
+    return new CorsWebFilter(source);
+  }
+}
