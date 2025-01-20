e6c312ce0817125afb072f6776ce849d4b02e744
varungarade1151@gmail.com <your.email@example.com>
2025-01-20 15:49:12 +0530

    main

--- api-gateway/src/main/java/com/micro/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/micro/ApiGatewayApplication.java
@@ -3,12 +3,14 @@ package com.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @SpringBootApplication()
 @RestController
+//@CrossOrigin("*")
 public class ApiGatewayApplication {
 
   public static void main(String[] args) {
